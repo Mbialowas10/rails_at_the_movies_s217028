7640237337ad43353d5abbcf2626ced0b548b10d
Michael Bialowas <mbialowas@rrc.ca>
2022-01-25 15:02:38 -0600

    Added Movies and ProductionCompanies

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'production_companies/show'
   get 'movies/index'
   get 'movies/show'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

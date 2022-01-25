7e264237679a912cfae1662c6812f3e5d6345a44
Michael Bialowas <mbialowas@rrc.ca>
2022-01-25 16:33:12 -0600

    added some real eye popping styling

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,9 @@
 class ProductionCompaniesController < ApplicationController
   def index
+    @production_companies = ProductionCompany.all
   end
 
   def show
+    @production_company = ProductionCompany.find(params[:id])
   end
 end

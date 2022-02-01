f966e257677d77f462fe81bea5be126e7fdb0db6
Michael Bialowas <mbialowas@rrc.ca>
2022-02-01 14:30:03 -0600

    Search Implemented

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,4 +9,8 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @movies = Movie.where("title LIKE ?", wildcard_search)
+  end
 end
